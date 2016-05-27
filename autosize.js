8393dd9b706f39940623884bbb5e54b10cb09b10
Alexey <oboshto@gmail.com>
2016-05-27 15:23:53 +0300

    Update autosize.js
    naming fix

--- autosize.js
+++ autosize.js
@@ -1,48 +1,48 @@
-function autoSizeEvt(evt) {
-	autoSize(evt.target);
+function autoSizeEvt(e) {
+    autoSize(e.target);
 }
 
-function windowResiseEvt(evt) {
-	resizeAllTextarea();
+function windowResizeEvt(e) {
+    resizeAllTextareas();
 }
 
-function resizeAllTextarea() {
-	var taArray = getTextareaArray();
-  taArray.forEach(function(ta) {
-  	autoSize(ta);
-  });
+function resizeAllTextareas() {
+    var taArray = getTextareasArray();
+    taArray.forEach(function (ta) {
+        autoSize(ta);
+    });
 }
 
-function getTextareaArray() {
-	var taArray = [];
-  var taList = document.getElementsByTagName('textarea');
-  for (var i = 0; i < taList.length; i++) {
-  	taArray.push(taList[i]);
-  }
-	return taArray;
+function getTextareasArray() {
+    var taArray = [];
+    var taList = document.getElementsByTagName('textarea');
+    for (var i = 0; i < taList.length; i++) {
+        taArray.push(taList[i]);
+    }
+    return taArray;
 }
 
 function autoSize(ta) {
-  var style = window.getComputedStyle(ta, null);
-  ta.style.height = 'auto';
-  ta.style.overflowY = 'hidden';
+    var style = window.getComputedStyle(ta, null);
+    ta.style.height = 'auto';
+    ta.style.overflowY = 'hidden';
 
-  var scrollHeight = ta.scrollHeight;
-  var offsetHeight;
-  
-  if (style.boxSizing === 'content-box') {
-			offsetHeight = -(parseFloat(style.paddingTop)+parseFloat(style.paddingBottom));
-		} else {
-			offsetHeight = parseFloat(style.borderTopWidth)+parseFloat(style.borderBottomWidth);
-		}
-    
-  ta.style.height = scrollHeight + offsetHeight + 'px';
+    var scrollHeight = ta.scrollHeight;
+    var offsetHeight;
+
+    if (style.boxSizing === 'content-box') {
+        offsetHeight = -(parseFloat(style.paddingTop) + parseFloat(style.paddingBottom));
+    } else {
+        offsetHeight = parseFloat(style.borderTopWidth) + parseFloat(style.borderBottomWidth);
+    }
+
+    ta.style.height = scrollHeight + offsetHeight + 'px';
 }
 
-var taArray = getTextareaArray();
-taArray.forEach(function(ta) {
-	autoSize(ta);
-	ta.addEventListener('input', autoSizeEvt);
-})
+var taArray = getTextareasArray();
+taArray.forEach(function (ta) {
+    autoSize(ta);
+    ta.addEventListener('input', autoSizeEvt);
+});
 
-window.addEventListener('resize', windowResiseEvt);
+window.addEventListener('resize', windowResizeEvt);
